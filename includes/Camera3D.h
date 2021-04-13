bfb090ac1c0addd4f7e3af871c5e80bf2925858d
gittem01 <mchtt43@gmail.com>
2021-04-14 00:29:08 +0300

    glm added separately

--- includes/Camera3D.h
+++ includes/Camera3D.h
@@ -4,7 +4,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glad/glad.h>
-#include <glfw3.h>
+#include <GLFW/glfw3.h>
 #include <math.h>
 #include "WindowPainter.h"
 
--- includes/WindowPainter.h
+++ includes/WindowPainter.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <glad/glad.h>
-#include <glfw3.h>
+#include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
 #include <imgui_impl_glfw.h>
